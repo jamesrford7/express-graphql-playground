2794ba3b1c782e2ad366cd4020f5101e75255b19
James Ford <jamesford@Jamess-MBP.default>
2017-01-15 17:43:34 +0000

    understand arg passing. add gitignore

--- server.js
+++ server.js
@@ -2,21 +2,24 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+const people = [{name: "John", profession: "coder"},
+                {name: "James", profession: "designer"}];
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
-    age: Int
+    hello: [String]
+    person(name: String): String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return 'Hello world!';
+    return ['Hello world!', 'Hello all'].map(function(greeting) {return greeting.toUpperCase()});
   },
-  age: () => {
-    return 5;
+  person: ({name}) => {
+    return people.find(function(person) { return person.name == name }).profession;
   }
 };
 
